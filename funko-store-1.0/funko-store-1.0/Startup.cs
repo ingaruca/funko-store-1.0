f8d5cfe3880bfc775d5316ee91d93d86926d7f27
Alejho139 <alejandro16_6@hotmail.com>
2018-06-04 11:41:37 -0500

    Inicializando Proyecto again

---
+++ funko-store-1.0/funko-store-1.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(funko_store_1._0.Startup))]
+namespace funko_store_1._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
