0f44e12839ea3c12b18f9f959525c6e61f9c1935
Alejho139 <alejandro16_6@hotmail.com>
2018-06-14 13:52:45 -0500

    INCLUYENDO EL PROYECTO DEL PROFE AL 90%

---
+++ funko-store-1.0/funko-store-1.0/Controllers/FunkoController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using funko_store_1._0.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Transactions;
+using System.Configuration;
+
+namespace funko_store_1._0.Controllers
+{
+    public class FunkoController : Controller
+    {
+        FUNKOBDEntities data = new FUNKOBDEntities();
+
+        #region Metodos
+
+
+
+        #endregion Metodos
+
+        #region Index
+        public ActionResult Index()
+        {
+            if (Session["carrito"] == null)
+            {
+                List<Registro> detalle = new List<Registro>();
+                Session["carrito"] = detalle;
+            }
+
+            return View(data.tb_productos.ToList());
+        }
+
+        #endregion Index
+
+        #region Seleccionar
+        public ActionResult Seleccionar(string id)
+        {
+            if (Session["carrito"] == null)
+            {
+                List<Registro> detalle = new List<Registro>();
+                Session["carrito"] = detalle;
+            }
+
+            return View(data.tb_productos.Where(x => x.idprodu == id).FirstOrDefault());
+        }
+
+        [HttpPost]
+        public ActionResult Seleccionar(string id,string nada )
+        {
+            tb_productos reg = data.tb_productos.Where(x => x.idprodu == id).FirstOrDefault();
+
+            Registro it = new Registro();
+            it.id = reg.idprodu;
+            it.nombre = reg.nomprodu;
+            it.precio = reg.precio;
+            it.cantidad = 1;
+            it.monto = reg.precio;
+
+            List<Registro> detalle = (List<Registro>)Session["carrito"];
+            detalle.Add(it);
+            Session["carrito"] = detalle;
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion Seleccionar
+
+        #region Comprar
+        public ActionResult Comprar()
+        {
+            List<Registro> detalle = (List<Registro>)Session["carrito"];
+            decimal mt = 0;
+            foreach (Registro it in detalle)
+            {
+                mt += it.monto;
+            }
+            ViewBag.mt = mt;
+            return View(detalle);
+        }
+
+        public ActionResult Eliminar(string id = null)
+        {
+            List<Registro> detalle = (List<Registro>)Session["carrito"];
+
+            foreach (Registro it in detalle)
+            {
+                if (it.id == id)
+                {
+                    detalle.Remove(it);
+                    break;
+                }
+            }
+
+            Session["carrito"] = detalle;
+            return RedirectToAction("comprar");
+        }
+
+        #endregion Comprar
+    }
+}
---
+++ funko-store-1.0/funko-store-1.0/Models/Registro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace funko_store_1._0.Models
+{
+    public class Registro
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public decimal precio { get; set; }
+        public int cantidad { get; set; }
+        public decimal monto { get; set; }
+    }
+}
