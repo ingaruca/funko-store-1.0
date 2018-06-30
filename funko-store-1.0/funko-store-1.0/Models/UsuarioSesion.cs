b2549906fecfff0d92e3fe9019e0287d5a719922
Luis Ingaruca <luise.ingaruca@gmail.com>
2018-06-29 19:16:20 -0500

    Se agrega modelo, controlador y vista para LOGIN y Usuario Sesion

---
+++ funko-store-1.0/funko-store-1.0/Models/UsuarioSesion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace funko_store_1._0.Models
+{
+    public class UsuarioSesion
+    {
+        public string idUsuSesion { get; set; }
+        public string nomUsuSesion { get; set; }
+    }
+}
